b41fbc42613810c79f2db00dacd10fd71b2cdca1
WesPalanca <wespalanca@gmail.com>
2024-10-26 15:15:25 -0400

    added journal feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import './App.css';
 import Auth from "./pages/auth";
 import Home from "./pages/Home";
+import Tracker from "./pages/Tracker";
 
 
 const App = () =>{
@@ -11,6 +12,7 @@ const App = () =>{
       <Routes>
         <Route path="/auth" element={<Auth />}></Route>
         <Route path="/Home" element={<Home />}></Route>
+        <Route path="/Tracker" element={<Tracker />}></Route>
       </Routes>
   </Router>
   )
