2e1d47eefd2294cc6d5f23faf39643fb8155d9fa
WesPalanca <wespalanca@gmail.com>
2024-10-26 22:21:21 -0400

    added single entry functionality

--- controllers/entry.js
+++ controllers/entry.js
@@ -6,7 +6,7 @@ export const getUserEntries = async (req, res) =>{
         const user = await Users.findById(userId);
         if (!user){
             console.log("user not found searching entries");
-            return res.status.json({success: false, message: "user not found searching entries"});
+            return res.status(404).json({success: false, message: "user not found searching entries"});
         }
         const allEntries = user.moodJournal;
         res.status(200).json({success: true, allEntries, message: "Got entries"});
@@ -14,5 +14,29 @@ export const getUserEntries = async (req, res) =>{
     catch(error){
         console.log(error);
         res.status(500).json({success: false, message: error});
+    }
+}
+
+export const getSingleUserEntry = async (req, res) =>{
+    try{
+        const userId = req.user.userId;
+        const user = await Users.findById(userId);
+        const entryId = req.query.entryId;
+        if (!user){
+            console.log("couldn't find user getting single user entry");
+            return res.status(404).json({success: false, message: "couldn't find user getting single user entry"});
+        }
+        const entry = user.moodJournal.id(entryId);
+        if (!entry) {
+            return res.status(404).json({ success: false, message: "Entry not found for this user" });
+        }
+        return res.status(200).json({ success: true, entry, message: "got single user entry" });
+        
+        
+
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({success: true, message: error})
     }
 }
--- routes/entries.js
+++ routes/entries.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { getUserEntries } from '../controllers/entry.js';
+import { getSingleUserEntry, getUserEntries } from '../controllers/entry.js';
 import { verifyToken } from '../middleware/auth.js';
 
 router.get('/entries/get', verifyToken, getUserEntries) //api/entries/get
+router.get('/entry/single', verifyToken, getSingleUserEntry) //api/entry/single
 
 export default router;
